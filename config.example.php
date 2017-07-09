f2cee4dcd335c8f06dd3f301243048922c9a7eaa
Fabio Souto <fabiolimasouto@gmail.com>
2017-07-08 23:40:47 -0300

    Start project bones

---
+++ config.example.php
@@ -0,0 +1,5 @@
+<?php
+return [
+	'downloads_dir' => '/home/fabio/Downloads/',
+	'sampler_dir' => '/prog/flsouto/sampler/'
+];
---
+++ utils.php
@@ -0,0 +1,64 @@
+<?php
+
+$config = require 'config.php';
+
+define('DOWNLOADS_DIR', $config['downloads_dir']);
+
+require $config['sampler_dir'].'/FlSouto/Sampler.php';
+
+use FlSouto\Sampler;
+
+function get_files($max=null){
+
+	$files = [];
+
+	foreach(scandir(DOWNLOADS_DIR) as $file){
+		if(stristr($file,'looperman') && stristr($file,'.wav')){
+			$file = DOWNLOADS_DIR.'/'.$file;
+			$files[filemtime($file)] = $file;
+		}
+	}
+
+	krsort($files);
+
+	if($max){
+		$files = array_slice($files,0,$max);
+	}
+
+	return $files;
+
+}
+
+
+function smp($file, $force120 = true){
+
+	$smp = new Sampler($file);
+	$len = $smp->len();
+
+	if(in_array($len,[4,8,16,32])){
+		return $smp;
+	}
+
+	if($len==12){
+		$smp->resize(rand(0,1)?8:16);
+	} else {
+
+		$closest = null;
+		$lastdiff = 999;
+
+		foreach([4,8,16,32] as $time){
+			$diff = abs($time-$len);
+			if($diff < $lastdiff){
+				$lastdiff = $diff;
+				$closest = $time;
+			}
+		}
+
+		$smp->resize($closest);
+
+	}
+
+	return $smp;
+
+
+}
