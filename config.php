fcba52d447d55136d6579ba5e7afaeec6975d496
Fabio Souto <fabiolimasouto@gmail.com>
2017-08-13 22:00:26 -0300

    add new files

---
+++ config.php
@@ -0,0 +1,5 @@
+<?php
+return [
+	'input_paths' => ['/home/fabio/Downloads/*.wav','/home/fabio/Downloads/noise/*/*.wav'],
+	'sampler_dir' => '/prog/flsouto/sampler/'
+];
---
+++ cycle.php
@@ -0,0 +1,104 @@
+<?php 
+
+require_once __DIR__.'/utils.php';
+
+if(empty($argv[1])){
+	die("Usage: command <SOURCE_FOLDER> [DESTINATION_FOLDER]\n");
+}
+
+$source = $argv[1];
+
+if(!empty($argv[2])){
+	$destin = "";
+}
+
+
+$files = glob($source."/*.wav");
+$stdin = fopen("php://stdin","r");
+
+$copied = "";
+
+if(!file_exists(__DIR__.'/copied')){
+	touch(__DIR__.'/copied');
+}
+
+$copied = file_get_contents(__DIR__.'/copied');
+
+while(true){
+
+	if(empty($files)){
+		echo "Done.\n";
+		break;
+	}
+
+	shuffle($files);
+
+	echo PHP_EOL;
+	echo " ============= OPTIONS ====================\n";
+	echo "|\n";
+	echo "|  rm + ENTER : remove sound\n";
+	echo "|  mv + ENTER : move to destination folder \n";
+	echo "|  r + ENTER : replay previous sound \n";
+	echo "|  cp destination + ENTER : copy this sound to destination\n";
+	echo "|  just ENTER : play next sound\n";
+	echo "|\n";
+	echo " ==========================================\n";
+	echo PHP_EOL;
+
+	foreach($files as $i => $file){
+
+		$basename = basename($file);
+
+		echo $basename;
+
+		echo ' -- ';
+		$smp = new FlSouto\Sampler($file);
+		$bpm = round(16 / $smp->len() * 120);
+		echo $bpm.'bpm';
+	
+		if(strstr($copied,$basename)){
+			echo ' **COPIED**';
+		}
+		
+		echo PHP_EOL;
+
+		play:
+		exec("play $file > /dev/null 2>&1 &");
+		
+		$input = trim(fgets($stdin));
+		$line = current(explode("\n",`ps aux | grep play`));
+		if(strstr($line,'.wav')){
+			preg_match("/(\d{3,})/",$line,$match);
+			shell_exec("kill ".$match[0]);
+			sleep(1);
+		}
+
+		if($input=='rm'){
+			shell_exec("rm $file");
+			unset($files[$i]);
+		}
+
+		else if(substr($input,0,3)=='cp '){
+			shell_exec("cp $file ".substr($input,3));
+			file_put_contents("copied",$basename."\n",FILE_APPEND);
+			$copied .= "\n".$basename;
+		}
+
+		else if($input=='mv'){
+			if(empty($destin)){
+				die("Destination folder was not given.\n");
+			} else {
+				shell_exec("mv $file $destination");
+				unset($files[$i]);
+			}
+		}
+
+		else if($input=='r'){
+			goto play;
+		}
+
+	}
+
+}
+
+
